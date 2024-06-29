1a44bc009a4200d4f02d6ffb38167f4e230ce336
bro299 <jeymarsidin@gmail.com>
2024-06-29 12:26:57 +0700

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const videoUrlInput = document.getElementById('videoUrl');
     const pasteBtn = document.getElementById('pasteBtn');
     const submitBtn = document.getElementById('submitBtn');
-    
 
     pasteBtn.addEventListener('click', (e) => {
         e.preventDefault();
@@ -15,43 +14,33 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 
-    function isValidUrl(url) {
-        const tiktokUrlPattern = /^(https?:\/\/)?(?:www\.)?tiktok\.com\/(?:v|@)?([\w-]+)/i;
-        return tiktokUrlPattern.test(url);
-    }
-
     submitBtn.addEventListener('click', (e) => {
         e.preventDefault();
         const videoUrl = videoUrlInput.value.trim();
 
-        if (isValidUrl(videoUrl)) {
-            fetch('/download', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    
-                },
-                body: JSON.stringify({ url: videoUrl })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.success) {
-                    window.location.href = data.downloadUrl;
-                } else {
-                    alert('Failed to download video. Please check the URL and try again.');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('Failed to download video. Please try again later.');
-            });
-        } else {
-            alert('Please enter a valid TikTok video URL.');
-        }
+        fetch('/download', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ url: videoUrl })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                window.location.href = data.downloadUrl;
+            } else {
+                alert('Failed to download video. Please check the URL and try again.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Failed to download video. Please try again later.');
+        });
     });
 });
